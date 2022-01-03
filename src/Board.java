53cb5b50efd851a23babac27d5f23b988bd57200
Mark <m.zhitchenko@student.utwente.nl>
2022-01-03 18:07:05 +0100

    3 commitment

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,16 @@ public class Board {
 
     public static final int RESOLUTION = 15;
 
+    private Square[][] squares;
 
+    public Board(Square[][] squares) {
+        this.squares = squares;
+    }
+
+    public void setSqare(int row, int col, Square.Type type) {
+
+
+
+    }
 
 }
--- src/Square.java
+++ src/Square.java
@@ -14,4 +14,6 @@ public class Square {
         this.type = type;
     }
 
+
+
 }
