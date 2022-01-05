b357ed73725d4cea2bdd878b38b9347c91a7f937
Dani <d.mahaini@student.utwente.nl>
2022-01-05 15:15:54 +0100

    Added class Player

---
+++ src/Player.java
@@ -0,0 +1,32 @@
+/**
+ * Abstract class for a player in scrabble game, will later on be subclassed by HumanPlayer and ComputerPlayer*/
+public abstract class Player {
+    private String name;
+    private int score;
+
+    /**
+     * Constructor that creates a player instance
+     * @requires name != null
+     * @ensures the Name of the player will be the parameter name */
+    public Player(String name){
+        assert name!= null;
+        this.name = name;
+    }
+
+    /**
+     * Getters and setters for the class variables
+     * Note: name only has a getter, because it is a read only property of the class*/
+    public String getName(){
+        return this.name;
+    }
+    public int getScore(){
+        return this.score;
+    }
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    public abstract String determineMove(Board board);
+
+
+}
