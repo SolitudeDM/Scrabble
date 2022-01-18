24e57e365488cf0ce51f1b511cd6cfe36bc0aeeb
Mark <m.zhitchenko@student.utwente.nl>
2022-01-18 17:17:32 +0100

    Updated the design

--- src/Model/players/HumanPlayer_v3.java
+++ src/Model/players/HumanPlayer_v3.java
@@ -4,6 +4,7 @@ import Model.Board;
 import Model.Game;
 import Model.Move;
 import Model.Tile;
+import View.utils.ANSI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,8 +22,38 @@ public class HumanPlayer_v3 extends Player {
     public String determineMove(Board board) {
 
         Scanner keyboard = new Scanner(System.in);
-        
-            System.out.println("enter your choice(command; coordinates; orientation; word)");
+
+        System.out.print(ANSI.WHITE_BRIGHT);
+        System.out.println("      Please enter your choice using this format: ");
+        System.out.print("To place a tile:");
+        System.out.print(ANSI.PURPLE_BOLD_BRIGHT);
+        System.out.print("PLACE;");
+        System.out.print(ANSI.PURPLE_BOLD);
+        System.out.print(" coordinates;");
+        System.out.print(ANSI.BLUE_BOLD);
+        System.out.print(" orientation;");
+        System.out.print(ANSI.BLUE_BOLD_BRIGHT);
+        System.out.print(" word;");
+
+        System.out.print(ANSI.WHITE_BRIGHT);
+        System.out.println();
+        System.out.print("      To swap tiles: ");
+        System.out.print(ANSI.PURPLE_BOLD_BRIGHT);
+        System.out.print("SWAP;");
+        System.out.print(ANSI.PURPLE_BOLD);
+        System.out.print(" tile1 ");
+        System.out.print(ANSI.BLUE_BOLD);
+        System.out.print(" tile2 ");
+        System.out.print(ANSI.BLUE_BOLD_BRIGHT);
+        System.out.print(" tile3 ...");
+
+        System.out.print(ANSI.WHITE_BRIGHT);
+        System.out.println();
+        System.out.print("              To exit the game: ");
+        System.out.print(ANSI.RED_BOLD_BRIGHT);
+        System.out.print("EXIT");
+        System.out.println();
+        System.out.print("                ");
 
             String choice = keyboard.nextLine();
             return choice;
